b298cb5d3c78c47172f29fa5bd5659ba4d57e280
Claeb1mb <52614744+Claeb1mb@users.noreply.github.com>
2020-06-12 20:55:51 -0400

    Testing

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useForm } from "../hooks/useForm";
 
 const initialValue = {
   firstName: "",
@@ -12,19 +13,12 @@ const initialValue = {
 // This form should be handled by a "useForm" custom hook
 // Build out the logic needed for a form custom hook (see the useForm.js file)
 // and replace the necessary stateful logic from CheckoutForm with the hook
-
-const CheckoutForm = (props) => {
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
-
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setShowSuccessMessage(true);
-  };
+export default function CheckoutForm() {
+  const [showSuccessMessage, values, handleChanges, handleSubmit] = useForm(
+    "checkoutForm",
+    initialValue,
+    showSuccessMessage
+  );
 
   return (
     <>
@@ -88,6 +82,4 @@ const CheckoutForm = (props) => {
       )}
     </>
   );
-};
-
-export default CheckoutForm;
+}
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,18 +1,32 @@
 // write your custom hook here to control your checkout form
 import { useState } from "react";
 
-export const UseForm = (key, initialValue) => {
-  const [storedValue, setStoredValue] = useState(() => {
-    const item = window.localStorage.getItem(key);
-    return item ? JSON.parse(item) : initialValue;
-  });
+export const useForm = (key, initialValue) => {
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [values, setValues] = useState(initialValue);
 
-  const setValue = (value) => {
-    setStoredValue(value);
-    window.localStorage.setItem(key, JSON.stringify(value));
+  const handleChanges = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
   };
 
-  return [storedValue, setValue];
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowSuccessMessage(true);
+  };
+  return [handleChanges, handleSubmit];
 };
 
-export default UseForm;
+export default useForm;
+
+/*const CheckoutForm = (props) => {
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [values, setValues] = useState(initialValue);
+  
+    const handleChanges = (e) => {
+      setValues({ ...values, [e.target.name]: e.target.value });
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      setShowSuccessMessage(true);
+    };*/
