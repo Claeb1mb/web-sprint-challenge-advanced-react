09db94d6ebd166ab7a97c88c20f2c2f8acd23d7b
Claeb1mb <52614744+Claeb1mb@users.noreply.github.com>
2020-06-13 13:38:31 -0400

    mvp

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import CheckoutForm from "./CheckoutForm";
+import CheckoutForm, { showSuccessMessage } from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
@@ -13,9 +13,7 @@ test("form header renders", () => {
   expect(header).toBeInTheDocument();
 });
 
-/*
 test("form shows success message on submit with form details", () => {
-  const { getByText } = render();
-
-  getByText();
-});*/
+  //AAA
+  expect(showSuccessMessage).toBe();
+});
