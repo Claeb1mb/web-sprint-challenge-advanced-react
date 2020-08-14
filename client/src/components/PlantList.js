a0d6308cc15d0388ed87d5ee778dc988c0360e92
Claeb1mb <52614744+Claeb1mb@users.noreply.github.com>
2020-08-14 19:44:47 -0400

    MVP Checkout & Plantlist

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -1,12 +1,30 @@
 import React, { Component } from "react";
 import axios from "axios";
+// add state with a property called "plants" - initialize as an empty array
+
+// when the component mounts:
+//   - fetch data from the server endpoint - http://localhost:3333/plants
+//   - set the returned plants array to this.state.plants
 
 export default class PlantList extends Component {
-  // add state with a property called "plants" - initialize as an empty array
+  constructor() {
+    super();
+    this.state = {
+      plants: [],
+    };
+  }
 
-  // when the component mounts:
-  //   - fetch data from the server endpoint - http://localhost:3333/plants
-  //   - set the returned plants array to this.state.plants
+  componentDidMount() {
+    axios
+      .get("http://localhost:3333/plants")
+      .then((res) => {
+        console.log("Plants Load", res.data.plantsData);
+        this.setState({ plants: res.data.plantsData });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
